8f8c5e0a45279c46af542fe9b6a14dc5921d4170
Akmal Izuddin <izuddin0407@gmail.com>
2022-02-23 22:07:41 +0800

    completed the logic for the form. whats left is implementing the education and professional experience section of the form. wanted to implement today but refactoring code for readability took a long time because of decisions lol

--- src/App.js
+++ src/App.js
@@ -10,122 +10,78 @@ class App extends Component {
   constructor( props ) {
     super();
     this.state = {
-      personalObject: {
-        firstName: '',
-        firstNameID: uniqid(),
-        lastName: '',
-        lastNameID: uniqid(),
-        email: '',
-        emailID: uniqid(),
-        phoneNum: '',
-        phoneNumID: uniqid(),
-      },
-      personalArray: [ {
-        firstName: 'First Name',
-        firstNameID: uniqid(),
-        lastName: 'Last Name',
-        lastNameID: uniqid(),
-        email: 'email@rocketmail.com',
-        emailID: uniqid(),
-        phoneNum: '012 3456 7890',
-        phoneNumID: uniqid(),
-      } ],
+      firstName: { text: '', id: uniqid() },
+      lastName: { text: '', id: uniqid() },
+      email: { text: '', id: uniqid() },
+      phoneNum: { text: '', id: uniqid() },
+
+      firstNameArray: [{ text: 'First Name', id: uniqid() }],
+      lastNameArray: [{ text: 'Last Name', id: uniqid() }],
+      emailArray: [{ text: 'email@rocketmail.com', id: uniqid() }],
+      phoneNumArray: [{ text: '012 3456 7890', id: uniqid() }],
     };
   }
 
   changeFirstName = (e) => {
     this.setState({
-      firstName: e.target.value,
+      firstName: { text: e.target.value, id: this.state.firstName.id },
     });
   };
 
   changeLastName = (e) => {
     this.setState({
-      lastName: e.target.value,
+      lastName: { text: e.target.value, id: this.state.firstName.id },
     });
   };
 
   changeEmail = (e) => {
     this.setState({
-      email: e.target.value,
+      email: { text: e.target.value, id: this.state.firstName.id },
     });
   };
 
   changePhoneNum = (e) => {
     this.setState({
-      phoneNum: e.target.value,
+      phoneNum: { text: e.target.value, id: this.state.firstName.id },
     });
   };
 
   onSubmitCV = (e) => {
     e.preventDefault();
     this.setState({
-      personalObject: {
-        firstName: '',
-        firstNameID: uniqid(),
-        lastName: '',
-        lastNameID: uniqid(),
-        email: '',
-        emailID: uniqid(),
-        phoneNum: '',
-        phoneNumID: uniqid(),
-      },
+      firstNameArray: [ this.state.firstName, ...this.state.firstNameArray ],
+      lastNameArray: [ this.state.lastName, ...this.state.lastNameArray ],
+      emailArray: [ this.state.email, ...this.state.emailArray ],
+      phoneNumArray: [ this.state.phoneNum, ...this.state.phoneNumArray ],
+      
+      firstName: { text: '', id: uniqid() },
+      lastName: { text: '', id: uniqid() },
+      email: { text: '', id: uniqid() },
+      phoneNum: { text: '', id: uniqid() },
     });
   };
 
   render() {
-    const { personalObject, personalArray } = this.state;
+    const { firstName, lastName, email, phoneNum,
+            firstNameArray, lastNameArray, emailArray, phoneNumArray } = this.state;
 
     return (
       <div className='main-container' >
         <Header text='CV MAKER APP' />
         <div className='content-container'>
           <form onSubmit={ this.onSubmitCV }>
-            <div className="personal-container">
-              <Header text='PERSONAL INFORMATION'/>
-              <div className='nameContainer'>
-                <input 
-                 onChange={ this.changeFirstName }
-                 value={ personalObject.firstName }
-                 type='text'
-                 id='firstName'
-                 placeholder='FIRST NAME'
-                 required
-                />
-                <input
-                 onChange={ this.changeLastName }
-                 value={ personalObject.lastName }
-                 type='text'
-                 id='lastName'
-                 placeholder='LAST NAME'
-                 required
-                />
-              </div>
-              <div className='contactContainer'>
-                <input
-                 onChange={ this.changeEmail }
-                 value={ personalObject.email }
-                 type='email'
-                 id='emailInput'
-                 placeholder='ENTER E-MAIL'
-                 required
-                />
-                <input
-                 onChange={ this.changePhoneNum }
-                 value={ personalObject.phoneNum }
-                 type='tel'
-                 id='phoneNo'
-                 minLength='8' 
-                 maxLength='16'
-                 placeholder='ENTER PHONE NUMBER'
-                 required
-                />
-              </div>
+            <Header text='ENTER YOUR INFO'/>
+            <div className='nameContainer'>
+              <input onChange={ this.changeFirstName } value={ firstName.text } type='text' id='firstName' placeholder='FIRST NAME' required/>
+              <input onChange={ this.changeLastName } value={ lastName.text } type='text' id='lastName' placeholder='LAST NAME' required/>
+            </div>
+            <div className='contactContainer'>
+              <input onChange={ this.changeEmail } value={ email.text } type='email' id='emailInput' placeholder='ENTER E-MAIL' required/>
+              <input onChange={ this.changePhoneNum } value={ phoneNum.text } type='tel' id='phoneNo' minLength='8'  maxLength='16' placeholder='ENTER PHONE NUMBER' required/>
             </div>
             <button type='submit'>SAVE CV</button>
           </form>
-          <ExampleCV firstName={ personalArray[0].firstName } lastName={ personalArray[0].lastName }
-           email={ personalArray[0].email } phoneNum={ personalArray[0].phoneNum }/>
+          <ExampleCV  fnArray={ firstNameArray } lnArray={ lastNameArray } eArray={ emailArray } pnArray={ phoneNumArray }/>
         </div>
       </div>
     );
--- src/components/ExampleCV.js
+++ src/components/ExampleCV.js
@@ -10,11 +10,11 @@ class ExampleCV extends Component {
             <div className="cv-container">
                 <Header text='PERSONAL INFORMATION'/>
                 <div className="userName">
-                    <p>{ this.props.firstName }</p>
-                    <p>{ this.props.lastName }</p>
+                    <p key={ this.props.fnArray[0].id }>{ this.props.fnArray[0].text }</p>
+                    <p key={ this.props.lnArray[0].id }>{ this.props.lnArray[0].text }</p>
                 </div>
-                <p>{ this.props.email }</p>
-                <p>{ this.props.phoneNum }</p>
+                <p key={ this.props.eArray[0].id }>{ this.props.eArray[0].text }</p>
+                <p key={ this.props.pnArray[0].id }>{ this.props.pnArray[0].text }</p>
             </div>
         )
     }
