992f0adf8fc2d918766422d2f1734731c47776b7
Akmal Izuddin <izuddin0407@gmail.com>
2022-02-27 09:18:00 +0800

    completed refatoring the code to functional components UI side of the project, whats left is implementing the logic side of the project. going to learn how to create react forms using functional components hooks first

--- src/App.js
+++ src/App.js
@@ -11,6 +11,18 @@ import './App.css';
 const App = () => {
   const [ firstName, setFirstName ] = useState( 'FIRST NAME' );
   const [ lastName, setLastName ] = useState( 'LAST NAME' );
+  const [ email, setEmail ] = useState( 'email@rocketmail.com' );
+  const [ phoneNum, setPhoneNum ] = useState( '012 3456 7890' );
+
+  const [ schoolName, setSchoolName ] = useState( 'EDUCATION INSTITUTE' );
+  const [ studyTitle, setStudyTitle ] = useState( 'TITLE OF STUDY' );
+  const [ eduStartDate, setEduStartDate ] = useState( '07/04/1998' );
+  const [ eduEndDate, setEduEndDate ] = useState( '02/27/2022' );
+
+  const [ companyName, setCompanyName ] = useState( 'COMPANY NAME' );
+  const [ positionTitle, setPositionTitle ] = useState( 'POSITION TITLE' );
+  const [ workStartDate, setWorkStartDate ] = useState( '07/04/2017' );
+  const [ workEndDate, setWorkEndDate ] = useState( '02/27/2022' );
 
   return (
     <div className='main-container'>
@@ -22,6 +34,20 @@ const App = () => {
           <EduForm/>
           <WorkForm/>
         </div>
+        <ExampleCV 
+        firstName={ firstName } 
+        lastName={ lastName } 
+        email={ email }
+        phoneNum={ phoneNum }
+        schoolName={ schoolName } 
+        studyTitle={ studyTitle }
+        eduStartDate={ eduStartDate }
+        eduEndDate={ eduEndDate }
+        companyName={ companyName }
+        positionTitle={ positionTitle }
+        workStartDate={ workStartDate }
+        workEndDate={ workEndDate }
+        />
       </div>
     </div>
   )
--- src/components/ExampleCV.js
+++ src/components/ExampleCV.js
@@ -12,40 +12,24 @@ const ExampleCV = ( props ) => {
         <p>{ props.firstName }</p>
         <p>{ props.lastName }</p>
       </div>
+      <p>{ props.email }</p>
+      <p>{ props.phoneNum }</p>
+      <Header text='EDUCATION INFORMATION'/>
+      <p>{ props.schoolName }</p>
+      <p>{ props.studyTitle }</p>
+      <div className='dates'>
+        <p>FROM: { props.eduStartDate }</p>
+        <p>TO: { props.eduEndDate }</p>
+      </div>
+      <Header text='PROFESSIONAL EXPERIENCE INFORMATION'/>
+      <p>{ props.companyName }</p>
+      <p>{ props.positionTitle }</p>
+      <div className='dates'>
+        <p>FROM: { props.workStartDate }</p>
+        <p>TO: { props.workEndDate }</p>
+      </div>
     </div>
   )
 };
 
 export { ExampleCV }
-
-/*
-class ExampleCV extends Component {
-    render() {
-        return (
-            <div className="cv-container">
-                <Header text='PERSONAL INFORMATION'/>
-                <div className="userName">
-                    <p key={ this.props.fnArray[0].id }>{ this.props.fnArray[0].text }</p>
-                    <p key={ this.props.lnArray[0].id }>{ this.props.lnArray[0].text }</p>
-                </div>
-                <p key={ this.props.eArray[0].id }>{ this.props.eArray[0].text }</p>
-                <p key={ this.props.pnArray[0].id }>{ this.props.pnArray[0].text }</p>
-                <Header text='EDUCATION INFORMATION'/>
-                <p key={ this.props.snArray[0].id }>{ this.props.snArray[0].text }</p>
-                <p key={ this.props.stArray[0].id }>{ this.props.stArray[0].text }</p>
-                <div className="dates">
-                    <p key={ this.props.esdArray[0].id }>From: { this.props.esdArray[0].text }</p>
-                    <p key={ this.props.eedArray[0].id }>To: { this.props.eedArray[0].text }</p>
-                </div>
-                <Header text='PROFESSIONAL EXPERINCE INFORMATION'/>
-                <p key={ this.props.cnArray[0].id }>{ this.props.cnArray[0].text }</p>
-                <p key={ this.props.ptArray[0].id }>{ this.props.ptArray[0].text }</p>
-                <div className="dates">
-                    <p key={ this.props.wsdArray[0].id }>From: { this.props.wsdArray[0].text }</p>
-                    <p key={ this.props.wedArray[0].id }>To: { this.props.wedArray[0].text }</p>
-                </div>
-            </div>
-        )
-    }
-}
-*/
